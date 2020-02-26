c0017395fad3c4fc880a1820df677a2df5465170
Nick Semerad <nicksemerad@gmail.com>
2020-02-25 20:52:42 -0700

    end of lecture

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,16 @@ class PagesController < ApplicationController
   end
 
   def new
-    @page = Page.new
+    @page = Page.new 
+  end
+
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      redirect_to pages_path
+    else
+      render :new 
+    end
   end
 
   def edit
@@ -16,8 +25,24 @@ class PagesController < ApplicationController
   end
 
   def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
   end
+
+
+
+  private
+    def page_params
+      params.require(:page).permit(:title, :author, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'static_pages/home'
+  # get 'static_pages/about'
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
     #HTTP requests = CRUD actions
   resources :pages 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
